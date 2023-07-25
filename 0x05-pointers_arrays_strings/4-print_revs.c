68dc98a42ca7d2839b1a5feb7feb6ffe1dc72cf0
anathi90 <anathigusha26@gmail.com>
2023-07-24 18:48:05 -0700

    print revs

---
+++ 0x05-pointers_arrays_strings/4-print_revs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	_putchar('\n');
+}
